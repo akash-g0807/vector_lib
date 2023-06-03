07729b92b30dc0cb6b21d7d6c24309ab35339589
Akash <ag1884@york.ac.uk>
2023-06-03 14:17:07 +0100

    made vector library headers and definitions

---
+++ src/vec3d.h
@@ -0,0 +1,42 @@
+
+#ifndef VECTOR_2D_H
+#define VECTOR_2D_H
+
+struct vec3d {
+    float x;
+    float y;
+
+    // Constructors
+    vec3d();
+
+    vec3d(float x, float y);
+ 
+    //Deconstructor
+    //~vec2d() = default;
+
+    // Vector Operations
+    void Log();                     // Print vector to console in i j component form
+    void Add(vec3d& v);             // Add vectors vector1.Add(vector2)
+    void Subtract(vec3d& v);        // Subtract vectors vector1.Subtract(vector2)
+    float Dot_Product(vec3d& v);    // Calculating Dot Product of 2 vectors vector1.Dot_Product(vector2)
+    void Scalar_Mult(float n);      // Scalar Multiplication of vectors
+    float Cross_Product(vec3d& v);  // Cross product magnitude of vectors
+    float magnitude();              // Calculating magnitute/length of vectors
+    vec3d Unit_Vector();            // Returns a unit vector of the current vector
+    vec3d& Normalize();             // Normalizes the vector
+    vec3d Normal();                 // Calculates the orthogonal vector of current object
+    vec3d Rotate();                 // Rotates vector according to angle given in radians
+
+
+    //Vector Operator Overloading
+    vec3d& operator = (const vec3d& v);
+    bool operator == (const vec3d& v);
+    bool operator != (const vec3d& v);
+    vec3d operator + (const vec3d& v);
+    vec3d operator - (vec3d& v);
+    vec3d operator * (float n);
+    vec3d operator / (float n);
+    vec3d operator - ();
+};
+
+#endif
